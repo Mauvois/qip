7afcb740b97d98c596daca524baffeb567e613be
Mauvois <roger.mauvois@gmail.com>
2023-10-23 23:04:03 +0200

    JWT transition seems ok

--- backend/qipu_api/views.py
+++ backend/qipu_api/views.py
@@ -6,40 +6,56 @@ from rest_framework import viewsets
 import requests
 from requests.exceptions import RequestException
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .permissions import IsOwner
 from .models import User, Media, Post, Event, Contacter, Attendee, Unique
 from .serializers import UserSerializer, MediaSerializer, PostSerializer, EventSerializer, ContacterSerializer, AttendeeSerializer, UniqueSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class BaseViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class PermissionMixin(object):
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+
+class UserViewSet(BaseViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class MediaViewSet(viewsets.ModelViewSet):
+class MediaViewSet(PermissionMixin, BaseViewSet):
     queryset = Media.objects.all()
     serializer_class = MediaSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(PermissionMixin, BaseViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventViewSet(BaseViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 
-class ContacterViewSet(viewsets.ModelViewSet):
+class ContacterViewSet(BaseViewSet):
     queryset = Contacter.objects.all()
     serializer_class = ContacterSerializer
 
 
-class AttendeeViewSet(viewsets.ModelViewSet):
+class AttendeeViewSet(BaseViewSet):
     queryset = Attendee.objects.all()
     serializer_class = AttendeeSerializer
 
 
-class UniqueViewSet(viewsets.ModelViewSet):
+class UniqueViewSet(BaseViewSet):
     queryset = Unique.objects.all()
     serializer_class = UniqueSerializer
